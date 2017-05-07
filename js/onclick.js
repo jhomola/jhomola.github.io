24584ad354afeef64141a2566b84eda7f8d500c8
jhomola <homola@wustl.edu>
2017-05-06 22:44:39 -0500

    Create onclick.js

---
+++ js/onclick.js
@@ -0,0 +1,7 @@
+<script>
+$(document).ready(function(){
+$('.markdown-block .sqs-block-content h3').css('cursor','pointer');
+$(".markdown-block .sqs-block-content h3").nextUntil("h3").slideToggle();
+$(".markdown-block .sqs-block-content h3").click(function() {$(this).nextUntil("h3").slideToggle();});
+  });
+</script>
