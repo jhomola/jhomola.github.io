6f3716f514b9a710ea8ff173c5f8c23952b38b7d
jhomola <homola@wustl.edu>
2017-05-06 22:55:24 -0500

    Delete onclick.js

--- js/onclick.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-$('.markdown-block .sqs-block-content h3').css('cursor','pointer');
-$(".markdown-block .sqs-block-content h3").nextUntil("h3").slideToggle();
-$(".markdown-block .sqs-block-content h3").click(function() {$(this).nextUntil("h3").slideToggle();});
-  })
