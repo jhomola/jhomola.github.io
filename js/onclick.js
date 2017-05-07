92838d2560f5e66c5ea43281f1dc2ca2e33931a1
jhomola <homola@wustl.edu>
2017-05-06 22:49:17 -0500

    Update onclick.js

--- js/onclick.js
+++ js/onclick.js
@@ -1,7 +1,5 @@
-<script>
 $(document).ready(function(){
 $('.markdown-block .sqs-block-content h3').css('cursor','pointer');
 $(".markdown-block .sqs-block-content h3").nextUntil("h3").slideToggle();
 $(".markdown-block .sqs-block-content h3").click(function() {$(this).nextUntil("h3").slideToggle();});
-  });
-</script>
+  })
